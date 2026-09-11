b248df2a51ed09542ab02bfd02d16e633c0363c9
felipenakai-maker <felipenakai@edu.senai.br>
2026-09-11 11:02:41 -0300

    Adicionar arquivos de projeto.

---
+++ Geometria.cs
@@ -0,0 +1,20 @@
+﻿namespace Polimorfismo
+{
+    internal class Geometria
+    {
+        public double CalcularArea(double Lado)
+        {
+            return Lado * Lado;
+        }
+
+        public double CalcularArea(double Base, double Altura)
+        {
+            return Base * Altura;
+        }
+
+        public double CalcularArea(double Raio, bool IsCirculo)
+        {
+            return Math.PI * Raio * Raio;
+        }
+    }
+}
