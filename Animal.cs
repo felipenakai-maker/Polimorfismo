4f14b24e5883da523b7119048eb4cf8902d35ac4
felipenakai-maker <felipenakai@edu.senai.br>
2026-09-11 11:22:10 -0300

    Atividade 3

---
+++ Animal.cs
@@ -0,0 +1,34 @@
+﻿namespace Polimorfismo
+{
+    public class Animal
+    {
+        public virtual void EmitirSom()
+            {
+                Console.WriteLine("Som genérico de animal...");
+            }
+        }
+
+        public class Leao : Animal
+        {
+            public override void EmitirSom()
+            {
+                Console.WriteLine("ROAR!");
+            }
+        }
+
+        public class Macaco : Animal
+        {
+            public override void EmitirSom()
+            {
+                Console.WriteLine("UGH UGH AAH AAH!");
+            }
+        }
+
+        public class Cobra : Animal
+        {
+            public override void EmitirSom()
+            {
+                Console.WriteLine("SSSSSSS...");
+            }
+        }
+}
