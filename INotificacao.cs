32aed20af1549f47875392086c72bc4f1ebd3adc
felipenakai-maker <felipenakai@edu.senai.br>
2026-09-11 11:48:44 -0300

    Atividade 4

---
+++ INotificacao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo
+{
+    public interface INotificacao
+    {
+        void Enviar();
+    }
+    public class NotificacaoEmail : INotificacao
+    {
+        public void Enviar()
+        {
+            Console.WriteLine("Enviando notificação por email...");
+        }
+
+        public void Enviar(string anexo)
+        {
+            Console.WriteLine($"Enviando email com anexo: {anexo}");
+        }
+    }
+
+    public class NotificacaoSMS : INotificacao
+    {
+        public void Enviar()
+        {
+            Console.WriteLine("Enviando notificação por SMS...");
+        }
+    }
+
+    public class NotificacaoPush : INotificacao
+    {
+        public void Enviar()
+        {
+            Console.WriteLine("Enviando notificação push para o app...");
+        }
+    }
+}
